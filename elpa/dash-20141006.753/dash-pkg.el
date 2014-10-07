525cdf2bff6b3e749fdc37bec96574ba186aa2e1
dabus <frohring.pierrehenry@gmail.com>
2014-10-07 15:49:35 +0200

    rm solirized from user/addons. Add it from marmalade.

---
+++ elpa/dash-20141006.753/dash-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dash" "20141006.753" "A modern list library for Emacs" 'nil :keywords '("lists"))
--- user/conf/theme.el
+++ user/conf/theme.el
@@ -1,2 +1,2 @@
-(add-to-list 'custom-theme-load-path emacs-color-theme-solarized-path)
+;; (add-to-list 'custom-theme-load-path emacs-color-theme-solarized-path)
 (load-theme 'solarized-dark t)
