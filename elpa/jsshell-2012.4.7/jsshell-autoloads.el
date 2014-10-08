f24a3ad133c31aa6888e0b1a22f7a70eb4cbb4c4
dabus <frohring.pierrehenry@gmail.com>
2014-10-08 10:16:05 +0200

    delete unused packages

--- elpa/jsshell-2012.4.7/jsshell-autoloads.el
+++
@@ -1,13 +0,0 @@
-;;; jsshell-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-(provide 'jsshell-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; jsshell-autoloads.el ends here
--- elpa/svg-clock-0.5/svg-clock-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; svg-clock-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (svg-clock) "svg-clock" "svg-clock.el" (20958 38725
-;;;;;;  0 0))
-;;; Generated autoloads from svg-clock.el
-
-(autoload 'svg-clock "svg-clock" "\
-Start/stop the svg clock.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("svg-clock-pkg.el") (20958 38725 261862
-;;;;;;  0))
-
-;;;***
-
-(provide 'svg-clock-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; svg-clock-autoloads.el ends here
