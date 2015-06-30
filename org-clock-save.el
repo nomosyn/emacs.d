554bb26efc681a6a926a353625deb3f5cc11d80e
Pierre-Henry Fröhring <pierrehenry.frohring@openmailbox.org>
2015-06-30 21:24:55 +0200

    Update

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - Pierre-Henrys-MacBook-Pro.local at <2015-06-07 Sun 15:19>
-(setq stored-clock-history '(("/Users/nomos/Documents/tasks.org" . 679) ("/Users/nomos/Documents/tasks.org" . 397) ("/Users/nomos/Documents/tasks.org" . 203) ("/Users/nomos/Documents/tasks.org" . 9)))
+;; org-persist.el - Pierre-Henrys-MacBook-Pro.local at <2015-06-20 Sat 14:40>
+(setq stored-clock-history '(("/Users/nomos/Documents/tasks.org" . 216) ("/Users/nomos/Documents/tasks.org" . 9)))
